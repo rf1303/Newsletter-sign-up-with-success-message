338637ea3c79b8b833aa7407b0aae28f0c2fc573
Ramiro Fernandez <pulgaramiro@gmail.com>
2025-04-10 12:25:17 -0300

    Inicio Repositorio

---
+++ README.md
@@ -0,0 +1,46 @@
+# Frontend Mentor - Newsletter sign-up form with success message solution
+
+This is a solution to the [Newsletter sign-up form with success message challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+**Note: Delete this note and update the table of contents based on what sections you keep.**
+
+## Overview
+
+![](./preview1440.jpeg)
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+- Javascript 
+
+- [Example resource 1](https://www.example.com) - This helped me for XYZ reason. I really liked this pattern and will use it going forward.
+- [Example resource 2](https://www.example.com) - This is an amazing article which helped me finally understand XYZ. I'd recommend it to anyone still learning this concept.
+
+
+## Author
+
+- Frontend Mentor - [@yourusername](https://www.frontendmentor.io/profile/yourusername)
+
---
+++ assets/js/main.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+  
+    const formContainer = document.querySelector(".form__container");
+    const formMailError = document.querySelector('.form__mail-error');
+    const formInput = document.querySelector('.form__input');
+    const buttonSuccess = document.querySelector('.buttons__message');
+    const layoutForm = document.querySelector('.layout')
+    const layoutSuccess = document.querySelector('.layout__success')
+    const validMailRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+ 
+
+    formInput.addEventListener('input', () => {
+        formMailError.style.display = "none"; 
+        formInput.classList.remove('form__input-error');
+    });
+
+    formContainer.addEventListener('submit', function (event) {
+       event.preventDefault();
+
+        if (!validMailRegex.test(formInput.value)) {
+
+            formMailError.style.display = "block"; 
+            formInput.classList.add('form__input-error');
+            formInput.value = '';
+
+        } else {
+            
+            formMailError.style.display = "none";
+            formInput.classList.remove('form__input-error');
+            layoutForm.classList.add('display-none');
+            layoutSuccess.classList.remove('display-none');
+        }
+    }); 
+
+    buttonSuccess.addEventListener('click', () => {
+        layoutSuccess.classList.add('display-none');
+        layoutForm.classList.remove('display-none');
+        formContainer.submit(); 
+        formInput.value = '';
+    });
+    
+});
